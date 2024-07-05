15e91355595bbe00f0f271028111b286bed2a2e3
Yashi vijay vargiya <66182066+yashivijayvargiya@users.noreply.github.com>
2024-07-05 15:25:47 +0530

    Update JwtController.java

--- Ecommerce_backend/src/main/java/com/yt/ecommerce/controller/JwtController.java
+++ Ecommerce_backend/src/main/java/com/yt/ecommerce/controller/JwtController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.yt.ecommerce.entity.JwtRequest;
 import com.yt.ecommerce.entity.JwtResponse;
 import com.yt.ecommerce.service.JwtService;
